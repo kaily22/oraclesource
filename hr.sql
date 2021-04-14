e9d295a247e6e9a3c5a4126afaadaf5539826150
seunghyunlee <kailyyy02@gmail.com>
2021-04-14 18:10:04 +0900

    select query

--- hr.sql
+++ hr.sql
@@ -1,7 +1,41 @@
 --hr에 있는 모든 테이블을 확인
 select * from tab;
 --employess 테이블의 모든 내용 확인
-select * from employees;
+select * from  employees;
 --employees 테이블의 first_name, last_name, job_id만 조회
 select first_name, last_name, job_id from employees;
 
+--사원번호가 176인 사람의 last_name과 부서 번호를 조회
+select LAST_NAME, DEPARTMENT_ID from employees where EMPLOYEE_ID= 176;
+
+--연봉이 12000 이상되는 직원들의 last_name 및 salary를 조회
+select LAST_NAME, SALARY from employees where salary >= 12000;
+
+--연봉이 5000에서  12000의 범위 이외인 사람들의 last_name 및 salary를 조회
+select LAST_NAME, SALARY from employees where salary < 5000 OR  salary > 12000;
+
+--20번, 50번 부서에서 근무하는 모든 사원들의 LAST_NAME 및 부서번호를 알파벳 순으로 조회
+SELECT LAST_NAME, DEPARTMENT_ID FROM EMPLOYEES WHERE DEPARTMENT_ID IN(20,50) 
+ORDER BY LAST_NAME, DEPARTMENT_ID;
+
+--커미션을 버는 모든 사원들의 LASTNAME, SALARY, COMMISSION_PCT를 조회
+SELECT LAST_NAME, SALARY, COMMISSION_PCT 
+FROM EMPLOYEES
+WHERE COMMISSION_PCT > 0 
+ORDER BY SALARY DESC, COMMISSION_PCT DESC;
+
+--SALARY이 2500, 3500, 7000이 아니며 직업이 SA_REP 혹은 ST_CLERK인 사원 조회
+SELECT * FROM EMPLOYEES WHERE SALARY NOT IN(2500,3500,7000) AND JOB_ID IN ('SA_REP','ST_CLERK');
+
+--고용일자가 2008-02-20 ~ 2008-05-01 사이에 고용된 사원들의 LASTNAME, EMPLOYEEID, HIREDATE를 조회한 후 
+--고용일자가 내림차순으로 정렬(날짜 데이터는 ''사이에 넣어서 처리)
+
+SELECT LAST_NAME, EMPLOYEE_ID, HIRE_DATE 
+FROM EMPLOYEES 
+WHERE HIRE_DATE BETWEEN '2008-02-20' AND  '2008-05-01' 
+ORDER BY HIRE_DATE DESC;
+
+--2004년도에 고용된 사람들의 LAST_NAME, HIRE_dATE를 조회한 후 입사일 기준으로 오름차순 정렬
+SELECT LAST_NAME, HIRE_DATE 
+FROM EMPLOYEES WHERE HIRE_dATE 
+BETWEEN '2004-01-01' AND'2004-12-31' ORDER BY HIRE_dATE;
--- scott.sql
+++ scott.sql
@@ -45,4 +45,71 @@ select
  
  --EMPNO, ENAME, JOB, 조회할 때 EMPNO의 오름차순으로 조회
  select empno, ename, job from emp order by empno ASC;
+ 
+ --부서번호의 오름차순과 급여의 내림차순 정렬하기
+ select* from emp order by deptno ASC, sal DESC;
+ 
+ select empno as EMPLOYEE_NO, ename as EMPLOYEE_NAME, mgr as MANAGER,
+ sal as SALARY, comm as COMMISSION, deptno as DEPARTMENT_NO
+ from emp
+ order by deptno DESC, ename;
+ 
+ --WHERE : 조건에 맞춰서 조회를 하고 싶을때
+ 
+ --부서번호가 30번인 사원 조회
+ select * from emp where deptno = 30;
+ 
+ --사원번호가 7782인 사원 정보 조회
+select * from emp where empno = 7782;
 
+--부서번호가 30이고 사원직책이 salesman인 사원 조회
+select * from emp where deptno = 30 AND job = 'SALESMAN';
+
+--부서번호가 30이거나 사원직책이 CLERK인 사원 조회
+select * from emp where deptno = 30 OR job = 'CLERK';
+
+
+
+
+--산술연산자 : + - * / 
+select * from emp where sal*12 = 36000;
+
+--비교연산자 : > < >= <= 
+
+--SAL의 값이 3000 이상인 사원 조회
+select * from emp where sal >= 3000;
+
+--급여가 2500이고, 직업이 analyst 사원 조회
+select * from emp where sal > 2500 AND job = 'ANALYST';
+
+--사원 이름의 첫 문자가 f와 같거나 뒤쪽인 것만 조회
+select * from emp where ename >= 'F';
+
+--직무가 manager, salesman, clerk인 사원 조회
+select * from emp where job = 'MANAGER' OR job = 'SALESMAN' OR job = 'CLERK';
+
+
+--등가 비교 연산자( =(동등), !=, <>, ^=, )
+--sal 3000이 아닌 사원정보 출력
+select * from emp where sal <> 3000;
+
+--논리 부정 연산자 : NOT
+select * from emp where NOT sal=3000;
+
+--IN : = 
+select * from emp where job IN ('MANAGER', 'SALESMAN', 'CLERK');
+
+select * from emp where job != 'MANAGER' AND job ^= 'SALESMAN' AND job <> 'CLERK';
+select * from emp where job NOT IN ('MANAGER', 'SALESMAN', 'CLERK');
+
+
+--부서번호가 10, 20 사원 조회 
+select * from emp where deptno IN (10,20);
+
+--BETWEEN A AND B : 특정 범위의 데이터를 조회
+
+--SAL 2000이상 - 3000 이하 
+select * from emp where sal between 2000 AND 3000;
+
+--SAL 2000이상 - 3000 이하가 아닌 사원 조회
+select * from emp where sal not between 2000 AND 3000;
